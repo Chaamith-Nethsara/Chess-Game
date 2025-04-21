eb2ab0b5d130e26789686f3b8398cfa76f53ea1f
Chaamith <chamithnethsara111@gmail.com>
2025-04-21 16:47:35 +0530

    Created rook java file

---
+++ src/Pieces/Rook.java
@@ -0,0 +1,47 @@
+package Pieces;
+
+import Game.Player;
+import Game.Type;
+
+public class Rook extends Piece{
+
+    public Type type;
+
+    public Rook(int x, int y, Player player){
+        super(x,y,player);
+        type = Type.ROOK;
+    }
+
+    public Type getType(){
+        return type;
+    }
+
+    @Override
+    public boolean isValidPath(int finalX, int finalY) {
+        int x_diff = x-finalX;
+        int y_diff = y-finalY;
+        boolean valid;
+        if(x_diff==0 && (y_diff>0 || y_diff <0)){
+            valid = true;
+        } else if (y_diff==0 && (x_diff>0 || x_diff<0)) {
+            valid = true;
+
+        }else{
+            valid = false;
+        }
+        return valid;
+    }
+
+    @Override
+    public int[][] drawPath(int startX, int startY, int finalX, int finalY) {
+        int x_diff = startX - finalX;
+        int y_diff = startY - finalY;
+        int [][] path={{1,2},{2,1}};
+        if(x_diff==0){
+
+        }else{
+
+        }
+        return path;
+    }
+}
