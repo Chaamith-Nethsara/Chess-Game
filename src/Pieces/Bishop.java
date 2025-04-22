47ef6eeb61d8273dc9dda5e44a975016514a0956
Chaamith <chamithnethsara111@gmail.com>
2025-04-22 19:37:06 +0530

    Did changes to the pawn class and bishop class

--- src/Pieces/Bishop.java
+++ src/Pieces/Bishop.java
@@ -1,10 +1,37 @@
 package Pieces;
 
+import Game.Player;
+import Game.Type;
+
 // class for the bishop piece.
-public class Bishop {
+public class Bishop extends Piece {
 
+    public Type type;
 
+    public Bishop(int x, int y, Player player){
+        super(x,y,player);
+        type = Type.BISHOP;
+    }
 
+    @Override     // to return the type of the piece
+    public Type getType() {
+        return type;
+    }
 
+    @Override
+    public boolean isValidPath(int finalX, int finalY) {
+        int x_diff = x-finalX;
+        int y_diff = y-finalY;
 
+        if(x_diff==y_diff){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public int[][] drawPath(int startX, int startY, int finalX, int finalY) {
+        return new int[0][];
+    }
 }
--- src/Pieces/Pawn.java
+++ src/Pieces/Pawn.java
@@ -1,4 +1,29 @@
 package Pieces;
 
-public class Pawn {
+import Game.Player;
+import Game.Type;
+
+public class Pawn extends  Piece{
+
+    public Type type;
+
+    public Pawn(int x, int y, Player player){
+        super(x,y,player);
+        type = Type.PAWN;
+    }
+
+    @Override
+    public Type getType() {
+        return type;
+    }
+
+    @Override
+    public boolean isValidPath(int finalX, int finalY) {
+        return false;
+    }
+
+    @Override
+    public int[][] drawPath(int startX, int startY, int finalX, int finalY) {
+        return new int[0][];
+    }
 }
