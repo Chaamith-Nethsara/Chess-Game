b2b97b264d9d904d868d17cf4d5ea033aa10bdc3
Chaamith <chamithnethsara111@gmail.com>
2025-04-22 19:42:13 +0530

    Made some changes to the bishop class

--- src/Pieces/Bishop.java
+++ src/Pieces/Bishop.java
@@ -20,18 +20,17 @@ public class Bishop extends Piece {
 
     @Override
     public boolean isValidPath(int finalX, int finalY) {
-        int x_diff = x-finalX;
-        int y_diff = y-finalY;
+        int x_diff = Math.abs(finalX-x) ;
+        int y_diff = Math.abs(finalY-y) ;
 
-        if(x_diff==y_diff){
-            return true;
-        }else{
-            return false;
-        }
+        return x_diff == y_diff;  // this returns true or false.
     }
 
     @Override
     public int[][] drawPath(int startX, int startY, int finalX, int finalY) {
-        return new int[0][];
+
+
+
+
     }
 }
